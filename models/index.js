278d687bd21bf2ff06eb016ff1944d173ef690ef
MChambersIV <chambersm025@gmail.com>
2022-02-11 11:16:13 -0800

    started seed.js for db seeds and modified index.js in model target all models contained therein, user.js simple typo

--- models/index.js
+++ models/index.js
@@ -1,8 +1,13 @@
 const User = require('./User');
 const Coffee = require('./Coffee');
+const Bean = require('./Bean');
+const Roast = require('./Roast');
+const Sweetener = require('./Sweetener');
 
 Coffee.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Coffee };
+
+
+module.exports = { User, Coffee, Bean, Roast, Sweetener };
