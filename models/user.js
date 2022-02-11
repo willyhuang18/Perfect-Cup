c65c3e2945bbf76252b261359241a8bae100809e
wiily <willyhuang18@gmail.com>
2022-02-10 20:32:56 -0800

    adding user model

--- models/user.js
+++ models/user.js
@@ -2,3 +2,36 @@
 const { Model, DataTypes } = require('sequelize');
 //import connection file 
 const sequelize = require('../config/connection');
+
+class User extends Model{};
+
+User.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',  
+    }
+)
