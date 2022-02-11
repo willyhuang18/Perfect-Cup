59bbfbd50d6430a9be069426f0c478a0e57133d3
wiily <willyhuang18@gmail.com>
2022-02-11 14:04:23 -0800

    change the utils to helper

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const utils = require('./utils/utils')
+const utils = require('./utils/helper')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
