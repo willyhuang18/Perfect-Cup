e774caccf3586dd1989051f03e56bb56405f4cee
wiily <willyhuang18@gmail.com>
2022-02-11 20:02:41 -0800

    undo the change willy make

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
-const utils = require('./utils/helper')
+// const utils = require('./utils/helper')
 
 const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,7 +12,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ utils });
+// const hbs = exphbs.create({ utils });
 
 // const sess = {
 //     secret: '69393absbshWHHHRshfd58cn0934d',
@@ -26,7 +26,7 @@ const hbs = exphbs.create({ utils });
 
 // app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+// app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
